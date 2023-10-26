2257b10af7a9cb87be8fd8ee867566ce719312e2
wilian.oliveiractt@gmail.com <lazoliver>
2023-10-25 22:04:42 -0300

    server log - health_handler log

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ use actix_web::{Responder, HttpResponse, App, web, HttpServer};
 
 
 async fn health_handler() -> impl Responder {
+    println!("Health handler execute sucessfuly");
     HttpResponse::Ok().body("{\"status\": \"pass\"}")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    println!("Server is running on http://localhost:4000");
     HttpServer::new(|| {
         App::new()
             .route("/health", web::get().to(health_handler))
