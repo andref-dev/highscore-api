769a2db02df01b1ebed7d8f6d474c105224e27c4
wilian.oliveiractt@gmail.com <lazoliver>
2023-10-26 13:51:27 -0300

    '/echo' endpoint

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,17 @@ async fn health_handler() -> impl Responder {
     HttpResponse::Ok().body("{\"status\": \"pass\"}")
 }
 
+async fn echo_handler(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Server is running on http://localhost:4000");
     HttpServer::new(|| {
         App::new()
             .route("/health", web::get().to(health_handler))
+            .route("/echo", web::post().to(echo_handler))
     })
     .bind(("127.0.0.1", 4000))?
     .run()
