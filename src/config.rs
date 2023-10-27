361a3402eac75d0d93a36bf091e27d68c3f8556e
Andre Ferreira dos Santos <andre.hl2@outlook.com>
2023-10-27 11:19:47 -0300

    - Adds Config object to the project

---
+++ src/config.rs
@@ -0,0 +1,30 @@
+use dotenv::dotenv;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, Clone)]
+#[serde(rename_all = "snake_case")]
+pub enum ReleaseMode {
+    Dev,
+    Prod
+}
+
+fn default_api_port() -> u16 {
+    4000
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct Config {
+    pub release_mode: ReleaseMode,
+    #[serde(default = "default_api_port")]
+    pub api_port: u16
+}
+
+
+impl Config {
+    pub fn new() -> Self {
+        dotenv().ok();
+        let config = envy::from_env::<Config>().expect("Error processing config object");
+
+        config
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use actix_web::{Responder, HttpResponse, App, web, HttpServer};
 
+use crate::config::Config;
+
+mod config;
 
 async fn health_handler() -> impl Responder {
     println!("Health handler executed successfully");
@@ -13,13 +16,17 @@ async fn echo_handler(req_body: String) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("Server is running on http://localhost:4000");
+
+    let config = Config::new();
+    
+    let startup_message = format!("Server is running on http://localhost:{}", config.api_port);
+    println!("{}", startup_message);
     HttpServer::new(|| {
         App::new()
             .route("/health", web::get().to(health_handler))
             .route("/echo", web::post().to(echo_handler))
     })
-    .bind(("127.0.0.1", 4000))?
+    .bind(("127.0.0.1", config.api_port))?
     .run()
     .await
 }
