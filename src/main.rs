6d7515206cc23cfd8cc2725f8c6efb89c6759d6c
wilian.oliveiractt@gmail.com <lazoliver>
2023-10-28 08:57:54 -0300

    - add a proper logs

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,30 @@
-use actix_web::{Responder, HttpResponse, App, web, HttpServer};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use env_logger::Env;
+use log::info;
 
 use crate::config::Config;
 
 mod config;
 
 async fn health_handler() -> impl Responder {
-    println!("Health handler executed successfully");
+    info!("Health handler executed successfully");
     HttpResponse::Ok().body("{\"status\": \"pass\"}")
 }
 
 async fn echo_handler(req_body: String) -> impl Responder {
-    println!("Echo handler executed successfully");
+    info!("Echo handler executed successfully");
     HttpResponse::Ok().body(req_body)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
     let config = Config::new();
-    
+
+    let env = Env::default().default_filter_or("info");
+    env_logger::init_from_env(env);
+
     let startup_message = format!("Server is running on http://localhost:{}", config.api_port);
-    println!("{}", startup_message);
+    info!("{}", startup_message);
     HttpServer::new(|| {
         App::new()
             .route("/health", web::get().to(health_handler))
