678ff4dc1a5c7ec072b23cf7fa26bc13e57982e2
wilian.oliveiractt@gmail.com <lazoliver>
2023-10-29 11:31:40 -0300

    - release mode corrections

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{Responder, HttpResponse, App, web, HttpServer};
 use env_logger::Env;
-use log::info;
+use log::{info, debug, warn};
+use config::ReleaseMode;
 
 use crate::config::Config;
 
@@ -12,19 +13,31 @@ async fn health_handler() -> impl Responder {
 }
 
 async fn echo_handler(req_body: String) -> impl Responder {
-    info!("Echo handler executed successfully");
+    info!("Echo handler executed successfully with data: {}", req_body);
     HttpResponse::Ok().body(req_body)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
     let config = Config::new();
 
-    let env = Env::default().default_filter_or("info");
-    env_logger::init_from_env(env);
+    let default_level = match config.release_mode {
+        ReleaseMode::Dev => "debug",
+        ReleaseMode::Prod => "info"
+    };
 
+    let env = Env::default().default_filter_or(default_level);
+    env_logger::init_from_env(env);
+    
     let startup_message = format!("Server is running on http://localhost:{}", config.api_port);
-    info!("{}", startup_message);
+    
+    match default_level {
+        "debug" => debug!("{}", startup_message),
+        "info" => info!("{}", startup_message),
+        _ => warn!("Not covered.")
+    };
+
     HttpServer::new(|| {
         App::new()
             .route("/health", web::get().to(health_handler))
