6335b06912e9b2daa75fda5b2573ee8dcaaadb4a
lazoliver <wilian.oliveiractt@gmail.com>
2023-11-12 02:12:57 -0300

    create-gamedev-script-execute

--- src/scripts/create_gamedev.rs
+++ src/scripts/create_gamedev.rs
@@ -1,15 +1,13 @@
 use crate::storage::storage::Storage;
 
 pub async fn execute(name: String) {
-    let mongo_uri = "mongodb://localhost:27017".to_string();
     let storage = Storage::new(mongo_uri).await.unwrap();
     match storage.create_gamedev(name).await {
         Ok(new_gamedev) => {
-            println!("Novo desenvolvedor de jogos inserido com sucesso:");
-            println!("{:?}", new_gamedev);
+            println!("GAMEDEV_ID: {:?}, API_KEY: {:?}", new_gamedev.id, new_gamedev.api_key);
         }
         Err(err) => {
-            println!("Erro ao criar o novo desenvolvedor de jogos: {:?}", err);
+            println!("Error to create a new GameDev: {:?}", err);
         }
     };
 }
