2754e9ba31dfcb66e835e64431d95a148aee5456
lazoliver <wilian.oliveiractt@gmail.com>
2023-11-13 17:57:05 -0300

    fix/script-create-gamedev-changes

--- src/scripts/create_gamedev.rs
+++ src/scripts/create_gamedev.rs
@@ -1,7 +1,9 @@
+use crate::Config;
 use crate::storage::storage::Storage;
 
 pub async fn execute(name: String) {
-    let storage = Storage::new(mongo_uri).await.unwrap();
+    let config = Config::new();
+    let storage = Storage::new(config.mongo_uri).await.unwrap();
     match storage.create_gamedev(name).await {
         Ok(new_gamedev) => {
             println!("GAMEDEV_ID: {:?}, API_KEY: {:?}", new_gamedev.id, new_gamedev.api_key);
