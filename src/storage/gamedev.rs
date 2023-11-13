d1e3affae955d3e85f52d3205932e68d862580a4
lazoliver <wilian.oliveiractt@gmail.com>
2023-11-13 14:21:39 -0300

    gamedev logger changes

--- src/storage/gamedev.rs
+++ src/storage/gamedev.rs
@@ -20,7 +20,7 @@ impl Storage {
         // Check if there's already a gamdev with this name
         match self.get_gamedev_by_name(name.clone()).await {
             Ok(_) => {
-                error!("The GameDev name exists.");
+                error!("Cannot create new gamedev because the name is already in use.");
                 return Err(AppError::DuplicateEntryError)
             },
             Err(_) => {}
@@ -33,7 +33,7 @@ impl Storage {
             api_key: Uuid::new_v4(),
         };
         self.gamedev_collection.insert_one(new_gamedev.clone(), None).await?;
-        debug!("{:?}", new_gamedev);
+        debug!("New gamedev created: {:?}", new_gamedev);
 
         // Return gamedev from DB
         self.get_gamedev_by_id(new_gamedev.id).await
@@ -45,25 +45,25 @@ impl Storage {
         // let filter = doc!{"id": id.to_string()};
         match self.gamedev_collection.find_one(filter, None).await? {
             Some(gamedev) => {
-                debug!("The GameDev search returned successfully.");
+                debug!("The GameDev search returned successfully with gamedev: {:?}.", gamedev);
                 Ok(gamedev)
             },
             None => {
-                error!("The GameDev not found.");
+                error!("Gamedev with id {} not found.", id);
                 Err(AppError::NotFound)
             }
         }
     }
 
     pub async fn get_gamedev_by_name(&self, name: String) -> Result<GameDev, AppError> {
-        let filter = doc!{"name": name};
+        let filter = doc!{"name": name.clone()};
         match self.gamedev_collection.find_one(filter, None).await? {
             Some(gamedev) => {
-                debug!("The GameDev search returned successfully.");
+                debug!("The GameDev search returned successfully with gamedev: {:?}.", gamedev);
                 Ok(gamedev)
             },
             None => {
-                error!("The GameDev not found.");
+                error!("Gamedev with name {} not found.", name);
                 Err(AppError::NotFound)
             }
         }
