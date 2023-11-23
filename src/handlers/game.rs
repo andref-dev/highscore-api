bde29d259b108adae367c9481de75da1b4977fcd
Andre Ferreira dos Santos <andre.hl2@outlook.com>
2023-11-22 22:33:15 -0300

    - Adds authorization for API KEY on required endpoints

--- src/handlers/game.rs
+++ src/handlers/game.rs
@@ -1,26 +1,15 @@
 use std::sync::RwLock;
 
-use actix_web::web::{self, Data};
+use actix_web::{web::{self, Data}, HttpRequest};
 use serde::{Serialize, Deserialize};
 use uuid::Uuid;
 use log::info;
 
-use crate::{error::AppError, storage::game::{NewGame, Game}, app_data::AppData};
-
-#[derive(Deserialize)]
-pub struct GetGameRequest {
-    gamedev_id: Uuid
-}
+use crate::{error::AppError, storage::game::{NewGame, Game}, app_data::AppData, handlers::utils::get_gamedev_id_from_request};
 
 #[derive(Deserialize)]
 pub struct CreateGameRequest {
-    gamedev_id: Uuid,
     name: String
-}
-
-#[derive(Deserialize)]
-pub struct GetAllGamesRequest {
-    gamedev_id: Uuid
 }
 
 #[derive(Serialize)]
@@ -43,27 +32,32 @@ pub struct AllGamesResponse {
     games: Vec<GameResponse>
 }
 
-pub async fn create_game_handler(new_game: web::Json<CreateGameRequest>, data: Data<RwLock<AppData>>) -> Result<web::Json<GameResponse>, AppError> {
+pub async fn create_game_handler(new_game: web::Json<CreateGameRequest>, request: HttpRequest, data: Data<RwLock<AppData>>) -> Result<web::Json<GameResponse>, AppError> {
+    let storage = &data.write()?.storage;
+    let gamedev_id = get_gamedev_id_from_request(&request, &storage).await?;
+
     let new_game = NewGame {
         name: new_game.name.clone(),
-        gamedev_id: new_game.gamedev_id
+        gamedev_id
     };
-    let storage = &data.write()?.storage;
+    
     let created_game = storage.create_game(new_game).await?;
     info!("Create Game Handler executed successfully.");
     Ok(web::Json(created_game.into()))
 }
 
-pub async fn get_game_handler(game_id: web::Path<Uuid>, req_body: web::Json<GetGameRequest>, data: Data<RwLock<AppData>>) -> Result<web::Json<GameResponse>, AppError> {
+pub async fn get_game_handler(game_id: web::Path<Uuid>, request: HttpRequest, data: Data<RwLock<AppData>>) -> Result<web::Json<GameResponse>, AppError> {
     let storage = &data.read()?.storage;
-    let game = storage.get_game_by_id(game_id.into_inner(), req_body.gamedev_id).await?;
+    let gamedev_id = get_gamedev_id_from_request(&request, &storage).await?;
+    let game = storage.get_game_by_id(game_id.into_inner(), gamedev_id).await?;
     info!("Get Game Handler executed successfully.");
     Ok(web::Json(game.into()))
 }
 
-pub async fn get_all_games_handler(gamedev: web::Json<GetAllGamesRequest>, data: Data<RwLock<AppData>>) -> Result<web::Json<AllGamesResponse>, AppError> {
+pub async fn get_all_games_handler(request: HttpRequest, data: Data<RwLock<AppData>>) -> Result<web::Json<AllGamesResponse>, AppError> {
     let storage = &data.read()?.storage;
-    let games = storage.get_games(gamedev.gamedev_id).await?;
+    let gamedev_id = get_gamedev_id_from_request(&request, &storage).await?;
+    let games = storage.get_games(gamedev_id).await?;
     let response = AllGamesResponse {
         games: games.into_iter().map(GameResponse::from).collect(),
     };
--- src/handlers/highscore.rs
+++ src/handlers/highscore.rs
@@ -1,22 +1,16 @@
 use std::sync::RwLock;
 
-use actix_web::web::{self, Data};
+use actix_web::{web::{self, Data}, HttpRequest};
 use serde::{Serialize, Deserialize};
 use uuid::Uuid;
 use log::info;
 
-use crate::{error::AppError, app_data::AppData, storage::highscore::{NewHighscore, Highscore}};
+use crate::{error::AppError, app_data::AppData, storage::highscore::{NewHighscore, Highscore}, handlers::utils::get_gamedev_id_from_request};
 
 #[derive(Clone, Deserialize)]
 pub struct UpdateHighscoreRequest {
-    gamedev_id: Uuid,
     score: u32,
     username: String
-}
-
-#[derive(Deserialize)]
-pub struct AllHighscoresRequest {
-    gamedev_id: Uuid
 }
 
 #[derive(Clone, Serialize)]
@@ -39,22 +33,26 @@ impl From<Highscore> for HighscoreResponse {
     }
 }
 
-pub async fn update_highscore_handler(new_highscore: web::Json<UpdateHighscoreRequest>, game_id: web::Path<Uuid>, data: Data<RwLock<AppData>>) -> Result<web::Json<HighscoreResponse>, AppError> {
+pub async fn update_highscore_handler(new_highscore: web::Json<UpdateHighscoreRequest>, game_id: web::Path<Uuid>, request: HttpRequest, data: Data<RwLock<AppData>>) -> Result<web::Json<HighscoreResponse>, AppError> {
+    let storage = &data.write()?.storage;
+    let gamedev_id = get_gamedev_id_from_request(&request, &storage).await?;
+
     let highscore = NewHighscore {
         game_id: game_id.into_inner(),
-        gamedev_id: new_highscore.gamedev_id,
+        gamedev_id: gamedev_id,
         score: new_highscore.score,
         username: new_highscore.username.clone()
     };
-    let storage = &data.write()?.storage;
+    
     let highscore = storage.update_highscore(highscore.clone()).await?;
     info!("Update Highscore Handler executed successfully.");
     Ok(web::Json(highscore.into()))
 }
 
-pub async fn get_highscores_handler(game_id: web::Path<Uuid>, req_body: web::Json<AllHighscoresRequest>, data: Data<RwLock<AppData>>) -> Result<web::Json<AllHighscoresResponse>, AppError> {
+pub async fn get_highscores_handler(game_id: web::Path<Uuid>, request: HttpRequest, data: Data<RwLock<AppData>>) -> Result<web::Json<AllHighscoresResponse>, AppError> {
     let storage = &data.read()?.storage;
-    let highscores = storage.get_highscores(game_id.into_inner(), req_body.gamedev_id).await?;
+    let gamedev_id = get_gamedev_id_from_request(&request, &storage).await?;
+    let highscores = storage.get_highscores(game_id.into_inner(), gamedev_id).await?;
     let response = AllHighscoresResponse {
         highscores: highscores.into_iter().map(HighscoreResponse::from).collect(),
     };
